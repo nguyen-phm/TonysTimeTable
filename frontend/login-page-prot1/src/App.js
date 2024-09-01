b982c6376bd1eb9f4f5eee5cba07f8a01b61fe55
Dave Gill <gillsd@student.unimelb.edu.au>
2024-09-01 13:10:33 +1000

    Added Button functions

--- frontend/login-page-prot1/src/App.js
+++ frontend/login-page-prot1/src/App.js
@@ -7,7 +7,87 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
+axios.defaults.xsrfCookieName = 'csrftoken';
+axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+axios.defaults.withCredentials = true;
+
+const client = axios.create({
+  baseURL: "http://127.0.0.1:8000/"
+});
+
 function App() {
+
+  const [currentUser, setCurrentUser] = useState();
+  const [registrationToggle, setRegistrationToggle] = useState(false);
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  useEffect(() => {
+    client.get("/testtoken")
+    .then(function(res) {
+      setCurrentUser(true);
+    })
+    .catch(function(error) {
+      setCurrentUser(false);
+    });
+  }, []);
+
+  function update_form_btn() {
+    if (registrationToggle) {
+      document.getElementById("form_btn").innerHTML = "Register";
+      setRegistrationToggle(false);
+    } else {
+      document.getElementById("form_btn").innerHTML = "Log in";
+      setRegistrationToggle(true);
+    }
+  }
+
+  function submitRegistration(e) {
+    e.preventDefault();
+    client.post(
+      "/signup",
+      {
+        email: email,
+        username: username,
+        password: password
+      }
+    ).then(function(res) {
+      client.post(
+        "/login",
+        {
+          email: email,
+          password: password
+        }
+      ).then(function(res) {
+        setCurrentUser(true);
+      });
+    });
+  }
+
+  function submitLogin(e) {
+    e.preventDefault();
+    client.post(
+      "/login",
+      {
+        email: email,
+        password: password
+      }
+    ).then(function(res) {
+      setCurrentUser(true);
+    });
+  }
+
+  function submitLogout(e) {
+    /*e.preventDefault();
+    client.post(
+      "/logout",
+      {withCredentials: true}
+    ).then(function(res) {
+      setCurrentUser(false);
+    });*/
+  }
 
   if (currentUser) {
     return (

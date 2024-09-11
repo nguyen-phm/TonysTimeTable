83d1feab6a11eff1fb9671055dbe570bbbfdfe39
Dave Gill <gillsd@student.unimelb.edu.au>
2024-09-11 11:31:48 +1000

    Added supabase API

--- backend backup/authentication/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import CustomUser
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta(object):
-        model = CustomUser
-        fields = ["id", "username", "password", "university_email", "personal_email", "is_student", "is_teacher"]
-        read_only_fields = ["is_student", "is_teacher"]

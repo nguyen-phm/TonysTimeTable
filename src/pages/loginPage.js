cf434a3e7f5c1a6c9e5ea323d30360ced6e191c0
njmuir <nicholas.muir03@gmail.com>
2024-11-03 16:10:42 +1100

    Fixed merge conflicts

--- src/pages/loginPage.js
+++ src/pages/loginPage.js
@@ -1,14 +1,9 @@
-<<<<<<< HEAD
 import LoginFormComponent from "../components/login/loginFormComponent";
-=======
-import LoginFormComponent from "../components/loginFormComponent";
-import TestComponent from "../components/testComponent";
->>>>>>> 79a98ae3b56dde14bb84fa06faf2173554d334a9
 
 function loginPage() {
     return (
       <div>
-          <TestComponent/>  
+          <LoginFormComponent/>  
       </div>
     ); 
   };
